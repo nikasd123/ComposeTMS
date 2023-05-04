b40ac62b95587938ebb59669449f14d019aefb4e
nikita <nikita_eremeev.95@mail.ru>
2023-05-04 13:14:15 +0300

    registration screen without button logic

--- app/src/main/java/com/example/composeproject/MainActivity.kt
+++ app/src/main/java/com/example/composeproject/MainActivity.kt
@@ -3,41 +3,62 @@ package com.example.composeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composeproject.ui.theme.ComposeProjectTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            RegistrationScreen()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun RegistrationScreen() {
+    var login by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeProjectTheme {
-        Greeting("Android")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(
+                value = login,
+                onValueChange = { login = it },
+                label = { Text(text = "Username:") },
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(text = "Password:") },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.padding(10.dp)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                onClick = { },
+                content = { Text(text = "Login") }
+            )
+        }
     }
 }
